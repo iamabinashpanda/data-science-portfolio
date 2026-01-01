84621a5f414f3d80ae01697cca04f0e1521ccf69
iamabinashpanda <iamabinashpanda@outlook.com>
2026-01-01 14:10:27 +0530

    deleteing file

--- Case Study 1/Question 5.py
+++
@@ -1,8 +0,0 @@
-# Design a code which will find whether the given number is Palindrome number or not
-
-num = input("Enter a number: ")
-
-if int(num) == int(num[::-1]):
-    print("It\'s a Palindrome number")
-else:
-    print("It\'s not a Palindrome number")
